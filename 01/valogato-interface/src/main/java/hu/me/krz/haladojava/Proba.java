a43f1cca744c55ab23ade1a920e372ecf8972764
nagydani98 <36605345+nagydani98@users.noreply.github.com>
2020-02-19 14:18:45 +0100

    finished

--- 01/valogato-interface/src/main/java/hu/me/krz/haladojava/Proba.java
+++ 01/valogato-interface/src/main/java/hu/me/krz/haladojava/Proba.java
@@ -1,10 +1,33 @@
 package hu.me.krz.haladojava;
 
-public class Proba {
+import java.util.Arrays;
 
+public class Proba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		if(args.length > 1) {
+			System.err.println("There is more than one argument!");
+			System.exit(-1);
+		}
+		int param = 0;
+		try {
+			param = Integer.parseInt(args[0]);
+			Szemely[] skacok = new Diak[param];
+			for (int i = 0; i < skacok.length; i++) {
+				skacok[i] = new Diak("skac", genNumBetween(10, 20), genNumBetween(0, 10));
+				
+				if(skacok[i].joAlanyE()) {
+					System.out.println(skacok[i]);			
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
 	}
 
+public static int genNumBetween(int lbound, int ubound) {
+	return (int) (Math.random() * (ubound - lbound + 1) + lbound);
+}
 }
--- 01/valogato-interface/src/main/java/hu/me/krz/haladojava/Szemely.java
+++ 01/valogato-interface/src/main/java/hu/me/krz/haladojava/Szemely.java
@@ -3,7 +3,7 @@ package hu.me.krz.haladojava;
 import lombok.Data;
 
 @Data
-public abstract class Szemely {
+public abstract class Szemely implements IValogato{
 	private String name;
 	private int age;
 	
