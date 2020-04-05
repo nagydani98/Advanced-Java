5cdc8d4777a928a8f37f129e374c701ec0d4a04c
nagydani98 <n.dani9812@gmail.com>
2020-04-05 17:20:22 +0200

    improved UserServiceImpl test

--- 01/usercontrollerdemo/src/test/java/dataTests/UserServiceImplTest.java
+++ 01/usercontrollerdemo/src/test/java/dataTests/UserServiceImplTest.java
@@ -1,8 +1,12 @@
 package dataTests;
 
-import static org.hamcrest.CoreMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +19,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import data.UserModify;
 import data.UserServiceImpl;
+import validators.SpaceValidator;
 import validators.Validator;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -36,6 +41,37 @@ class UserServiceImplTest {
 	@Test
 	void testConstructor() {
 		assertNotNull(usrService);
+	}
+	
+	@Test
+	void testSaveWithValidTrue(){
+		SpaceValidator sv = mock(SpaceValidator.class);
+		when(sv.isValid(null)).thenReturn(true);
+		List<Validator> validators = new ArrayList<Validator>();
+		validators.add(sv);
+		
+		usrService = new UserServiceImpl(repo, validators);
+		
+		usrService.saveUser(null);
+		
+		//saveUser validates and calls repo.save
+		//using mock, validator will always return true
+		//we only have to check if the save() method of repo is called
+		verify(repo, times(1)).save(null);
+	}
+	
+	@Test
+	void testSaveWithValidFalse(){
+		SpaceValidator sv = mock(SpaceValidator.class);
+		when(sv.isValid(null)).thenReturn(false);
+		List<Validator> validators = new ArrayList<Validator>();
+		validators.add(sv);
+		
+		usrService = new UserServiceImpl(repo, validators);
+		
+		usrService.saveUser(null);
+		
+		verify(repo, times(0)).save(null);
 	}
 
 }
