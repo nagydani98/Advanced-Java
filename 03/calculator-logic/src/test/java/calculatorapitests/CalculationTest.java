c94d6a3845909b04c31d03706705d36424cd5cc2
nagydani98 <n.dani9812@gmail.com>
2020-04-20 12:19:19 +0200

    finished unit tests with 92% coverage

--- 03/calculator-logic/src/test/java/calculatorapitests/CalculationTest.java
+++ 03/calculator-logic/src/test/java/calculatorapitests/CalculationTest.java
@@ -13,4 +13,53 @@ class CalculationTest {
 		
 		assertNotNull(calc);
 	}
+	
+	@Test
+	void testGettersSetters() {
+		int a = 2;
+		int b = 5;
+		char c = '+';
+		
+		Calculation calc = new Calculation();
+		calc.setLeftOperand(a);
+		calc.setRightOperand(b);
+		calc.setOperator(c);
+		
+		assertEquals(calc.getLeftOperand(), a);
+		assertEquals(calc.getRightOperand(), b);
+		assertEquals(calc.getOperator(), c);
+	}
+	
+	@Test
+	void testToString() {
+		Calculation calc = new Calculation();
+		
+		assertNotNull(calc.toString());
+	}
+	
+	@Test
+	void testHashCode(){
+		Calculation calc = new Calculation();
+		
+		assertNotNull(calc.hashCode());
+	}
+	
+	@Test
+	void testEquals() {
+		int a = 2;
+		int b = 5;
+		char c = '+';
+		
+		Calculation calc = new Calculation();
+		calc.setLeftOperand(a);
+		calc.setRightOperand(b);
+		calc.setOperator(c);
+		
+		Calculation calc2 = new Calculation();
+		calc2.setLeftOperand(a);
+		calc2.setRightOperand(b);
+		calc2.setOperator(c);
+		
+		assertTrue(calc.equals(calc2));
+	}
 }
