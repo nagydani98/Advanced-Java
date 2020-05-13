ed35de562521e07af64fa439468c9bc226aae16a
nagydani98 <n.dani9812@gmail.com>
2020-05-13 15:47:19 +0200

    init

---
+++ 04/newsletter/src/test/java/hu/me/krz/haladojava/newsletter/NewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.me.krz.haladojava.newsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
